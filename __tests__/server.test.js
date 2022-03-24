b810a6c698e6be1a186a454408a29fef5c8458a3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-23 17:37:32 -0700

    Added invalid route and invalid method test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,13 +1,21 @@
 'use strict';
 
 const supertest = require('supertest');
-const server = require('../server/app.js');
+const server = require('../src/server.js');
 const request = supertest(server.app);
 
 
 describe('Testing routing and handling of the API', () => {
 
-  test('Should test something', async () => {
-    await expect(true).toEqual(false);
+  test('API responds with 404 to invalid route', async () => {
+    let response = await request.get('/');
+
+    expect(response.status).toEqual(404);
+  });
+
+  test('API responds with 404 to invalid method', async () => {
+    let response = await request.patch('/game');
+
+    expect(response.status).toEqual(404);
   });
 });
