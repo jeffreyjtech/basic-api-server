0a93d422a16fd41add12fa77491c9765e73475e3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-23 20:46:33 -0700

    Added post and both get handlers

--- src/routes/platform.js
+++ src/routes/platform.js
@@ -7,13 +7,50 @@ const { PlatformModel } = require('../models');
 const router = express.Router();
 
 async function handlePostPlatform (req, res, next) {
+  // receive Game data from the client's HTTP request
+  try {
+    let newPlatform = req.body;
+  
+    // query to the DB
+    let responseData = await PlatformModel.create(newPlatform);
 
-  let newPlatformData = req.body;
-
-  let responseData = await PlatformModel.create(newPlatformData);
-
-  res.send(201).send(responseData);
+    res.status(201).send(responseData);
+  } catch (err) {
+    next(err);
+  }
 }
+
+async function handleGetAllPlatforms (req, res, next) {
+  try {
+    let responseData = await PlatformModel.findAll();
+    res.status(200).send(responseData);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function handleGetPlatform (req, res, next) {
+  try{
+
+    let id = parseInt(req.params.id);
+
+    let options = {where: {id: id}};
+    
+    console.log('Attempting to find one model with ID:', id);
+
+    let responseData = await PlatformModel.findOne(options);
+    
+    console.log(responseData);
+
+    res.status(200).send(responseData);
+  } catch (err) {
+    next(err);
+  }
+}
+
+router.post('/platform', handlePostPlatform);
+router.get('/platform', handleGetAllPlatforms);
+router.get('/platform/:id', handleGetPlatform);
 
 router.post('/platform', handlePostPlatform);
 
